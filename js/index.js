f0dc090d439273219ecee987865c6edefda9553f
David Carlisle <dave_0021@yahoo.com>
2020-09-10 10:50:14 -0500

    Update

--- js/index.js
+++ js/index.js
@@ -1,9 +1,26 @@
-const myButton = document.getElementById('clickMe');
-const noun1 = document.getElementById('noun1');
-const noun1Input = document.getElementById('noun1Input');
+const myButton = document.getElementById("clickMe");
+const noun1 = document.getElementById("noun1");
+const noun1Input = document.getElementById("noun1Input");
+const noun2 = document.getElementById("noun2");
+const noun2Input = document.getElementById("noun2Input");
+const placeholder = document.getElementsByClassName("placeholder");
 
-myButton.addEventListener('click', function (event) {
+myButton.addEventListener("click", function (event) {
   event.preventDefault();
 
-  noun1.innerHTML = noun1Input.value;
+  placeholder[0].textContent = noun1Input.value;
+  placeholder[1].textContent = noun2Input.value;
 });
+
+// myButton.addEventListener("click", function (event) {
+//   event.preventDefault();
+
+//   noun1.innerHTML = noun1Input.value;
+//   noun2.innerHTML = noun2Input.value;
+// });
+
+// myButton.addEventListener("click", function (event) {
+//   event.preventDefault();
+//   placeholder.innerHTML = noun1Input.value;
+//   placeholder.innerHTML = noun2Input.value;
+// });
